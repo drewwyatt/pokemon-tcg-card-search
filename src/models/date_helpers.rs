f28db8043c6c66c5f68f813a9fc53ead147850b2
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-01-01 17:08:34 -0500

    Move DateFormatType to a different file

---
+++ src/models/date_helpers.rs
@@ -0,0 +1,27 @@
+use chrono::{NaiveDate, NaiveDateTime};
+use chrono::format::{DelayedFormat, StrftimeItems, ParseResult};
+
+/// Types implementing this trait can convert to and from a string
+/// using a format string.
+pub trait DateFormatType {
+    fn format<'a>(&self, fmt: &'a str) -> DelayedFormat<StrftimeItems<'a>>;
+    fn parse_from_str(s: &str, fmt: &str) -> ParseResult<Self> where Self: Sized;
+}
+
+impl DateFormatType for NaiveDateTime {
+    fn format<'a>(&self, fmt: &'a str) -> DelayedFormat<StrftimeItems<'a>> {
+        self.format(fmt)
+    }
+    fn parse_from_str(s: &str, fmt: &str) -> ParseResult<Self> where Self: Sized {
+        Self::parse_from_str(s, fmt)
+    }
+}
+
+impl DateFormatType for NaiveDate {
+    fn format<'a>(&self, fmt: &'a str) -> DelayedFormat<StrftimeItems<'a>> {
+        self.format(fmt)
+    }
+    fn parse_from_str(s: &str, fmt: &str) -> ParseResult<Self> where Self: Sized {
+        Self::parse_from_str(s, fmt)
+    }
+}
