2c6373554d6d601f279824fff7e3453260650ff2
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-01-01 17:08:34 -0500

    Move DateFormatType to a different file

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,9 @@
+mod date_helpers;
 mod fetch_state;
 mod pokemon_error;
 mod set;
 
+pub use date_helpers::DateFormatType;
 pub use fetch_state::FetchState;
 pub use pokemon_error::PokemonError;
 pub use set::{Set, SetResponse};
